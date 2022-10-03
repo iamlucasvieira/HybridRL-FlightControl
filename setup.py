ad7f3088e4967101879f57bdca3b059402aef2f7
lucas <lucas6eng@gmail.com>
2022-10-03 10:56:08 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Preliminary work for my control systems with RL MSc thesis',
+    author='Lucas Vieira dos Santos',
+    license='MIT',
+)
