48eb9a96bd47f3fe827007ffb3604c034389f41c
lucas <lucas6eng@gmail.com>
2022-11-01 18:19:47 +0100

    Playground files

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,14 +6,10 @@ from dotenv import find_dotenv, load_dotenv
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+def main() -> None:
+    """ Nothing
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    pass
 
 
 if __name__ == '__main__':
