ba54595dcb0892439ca8ca65fbe2fe7736ad7bff
lucas <lucas6eng@gmail.com>
2022-11-21 09:41:20 -0300

    Add sac pendulum

---
+++ src/sandbox/sac_pendulum.py
@@ -0,0 +1,16 @@
+import gym
+from stable_baselines3 import SAC
+
+env = gym.make('Pendulum-v1')
+model = SAC('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=20_000)
+
+obs = env.reset()
+
+for _ in range(100):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, rewards, done, info = env.step(action)
+    env.render()
+
+    if done:
+        break
--- src/thesis_pilot/aircraft_aoa_hold.py
+++ src/thesis_pilot/aircraft_aoa_hold.py
@@ -1,21 +1,22 @@
 from models.aircraft_environment import AircraftEnv
 from stable_baselines3 import SAC
-from stable_baselines3.common.env_checker import check_env
+# from stable_baselines3.common.env_checker import check_env
+import matplotlib.pyplot as plt
 
-TRAIN = False
+TRAIN = True
 env = AircraftEnv('citation.yaml', dt=0.01, T=100)
 
 if TRAIN:
     # check_env(env)
     model = SAC("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=150_000, log_interval=100)
-    model.save("sac_pendulum")
+    model.learn(total_timesteps=int(100000), log_interval=100)
+    model.save("citation_sac")
 else:
-    model = SAC.load("sac_pendulum")
+    model = SAC.load("citation_sac")
 
 obs = env.reset()
 print("Done")
-import matplotlib.pyplot as plt
+
 for _ in range(10):
     states = []
     for i in range(20000):
