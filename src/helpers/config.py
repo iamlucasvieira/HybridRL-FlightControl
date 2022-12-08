691f52003725de681b424bd3ddd859e817411555
lucas <lucas6eng@gmail.com>
2022-12-08 15:29:11 +0100

    Use pydantic for config instead of dataclass

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,26 +1,34 @@
 """Module that define configuration of algorithms."""
+from pydantic import BaseModel, validator
+import numpy as np
+from typing import Optional
 
-from dataclasses import dataclass, asdict
 
-
-@dataclass
-class ConfigLinearAircraft:
-    """Class that defines the configuration of the linear aircraft."""
-
-    # General
-
+class ConfigLinearAircraft(BaseModel):
+    """Symmetric derivatives."""
     policy_type: str = "MlpPolicy"
-    env_name: str = "citation"
-    filename: str = "citation.yaml"
-    configuration: str = "symmetric"
-    algorithm: str = ""
-    seed: int = 1  # Random seed
-    dt: float = 0.1  # Time step
-    episode_steps: int = 100  # Number of steps
-    learning_steps: int = 1000  # Number of total learning steps
-    task: str = "aoa"
+    env_name: Optional[str] = "citation"
+    filename: Optional[str] = "citation.yaml"
+    configuration: Optional[str] = "symmetric"
+    algorithm: Optional[str] = ""
+    seed: Optional[int] = np.random.randint(1_000)  # Random seed
+    dt: Optional[float] = 0.1  # Time step
+    episode_steps: Optional[int] = 100  # Number of steps
+    learning_steps: Optional[int] = 1000  # Number of total learning steps
+    task: Optional[str] = "aoa"
 
-    @property
-    def asdict(self):
-        """Return class as dict"""
-        return asdict(self)
+    @validator('configuration')
+    def check_config(cls, configuration):
+        CONFIGURATIONS = ['symmetric', 'sp', 'asymmetric']  # Available aircraft configurations
+        if configuration not in CONFIGURATIONS:
+            raise ValueError(f"Configuration must be in {CONFIGURATIONS}")
+
+        return configuration
+
+    @validator('task')
+    def check_task(cls, task):
+        TASKS = ['aoa', 'aoa_sin']
+        if task not in TASKS:
+            raise ValueError(f"Task must be in {TASKS}")
+
+        return task
