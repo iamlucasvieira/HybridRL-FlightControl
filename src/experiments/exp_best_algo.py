ae3d661c829d03623d513a9ae81546d28fd9f22a
lucas <lucas6eng@gmail.com>
2022-12-08 17:04:32 +0100

    Add random seed to experiment

--- src/experiments/exp_best_algo.py
+++ src/experiments/exp_best_algo.py
@@ -1,16 +1,22 @@
 """Experiment to determine the best algorithm for the linear aircraft environment."""
 
 from experiments.core import Experiment
+import numpy as np
 
 
 def main(task="aoa_sin", project_name="best_algo", episode_steps=100, learning_steps=1_000, run=1):
+    """Run the experiment."""
+
+    np.random.seed(0)
     exp_sac = Experiment(
+
         project_name=project_name,
         algorithm_name="SAC",
         task_name=task,
         episode_steps=episode_steps,
         learning_steps=learning_steps,
-        run=run
+        run=run,
+        seed=np.random.randint(1_000),
     )
 
     exp_sac.learn()
@@ -21,13 +27,15 @@ def main(task="aoa_sin", project_name="best_algo", episode_steps=100, learning_s
         project_name=project_name,
         algorithm_name="TD3",
         task_name=task,
-        episode_steps = episode_steps,
-        learning_steps = learning_steps,
-        run=run
+        episode_steps=episode_steps,
+        learning_steps=learning_steps,
+        run=run,
+        seed=np.random.randint(1_000),
     )
 
     exp_td3.learn()
     exp_td3.finish_wandb()
+
 
 if __name__ == '__main__':
     main()
