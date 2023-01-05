6393b26fae38b817f937a11c9750e81aab4594ee
lucas <lucas6eng@gmail.com>
2023-01-05 10:46:52 +0100

    Make DSAC use greedy step

--- src/agents/seres_dsac.py
+++ src/agents/seres_dsac.py
@@ -140,7 +140,7 @@ class DSAC:
             print(text)
 
     def predict(self, observation, state=None, mask=None, deterministic=False):
-        return self.agent.act(observation), observation
+        return self.agent.act_greedy(observation), observation
 
     def save(self, file_path):
         self.agent.save_to_file(file_path)
