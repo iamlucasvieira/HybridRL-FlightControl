206a0cf76a04f7e0592e95dbdacd8ca4efb87c79
Lucas dos Santos <lucas6eng@gmail.com>
2023-01-17 16:41:04 +0100

    Add new dta for best reward

--- src/data/get_wandb_data.py
+++ src/data/get_wandb_data.py
@@ -4,37 +4,53 @@ import pandas as pd
 
 
 def get_wandb_data(project_name):
-    file_path = Path.data / f"{project_name}.csv"
-    file_path_rollout = Path.data / f"{project_name}_rollout.csv"
+    """Function that gathers the episode and learning data from wandb."""
 
-    if file_path.is_file():
-        print(f"File {file_path} already exists")
+    def get_columns(_run, keys):
+        """Return a dataframe with the passed_keys"""
+        return _run.history(keys=keys)[keys]
+
+    file_path_episode = Path.data / f"{project_name}_episode.csv"
+    file_path_learning = Path.data / f"{project_name}_learning.csv"
+
+    if file_path_episode.is_file() or file_path_learning.is_file():
+        print(f"File {file_path_episode} or {file_path_learning} already exists")
     else:
-        api = wandb.Api(timeout=19)
+        api = wandb.Api(timeout=29)
         runs = api.runs(f"lucasv/{project_name}")
-        df_tracking, df_rollout = pd.DataFrame(), pd.DataFrame()
+        df_all_episode, df_all_learning = pd.DataFrame(), pd.DataFrame()
         for run in runs:
-            # Tracking data
-            history = run.history(keys=["episode_step", "tracking_error"])
+            # Episode data
+            episode_data_list = [
+                get_columns(run, ["episode_step", "tracking_error"]),
+                get_columns(run, ["episode_step", "state", "reference"]),
+                get_columns(run, ["episode_step", "reward"]),
+                get_columns(run, ["episode_step", "action"]),
+            ]
 
-            # Rollout data
-            ep_len = run.history(keys=["rollout/ep_len_mean"])
-            ep_rew = run.history(keys=["rollout/ep_rew_mean"])
-            rollout = pd.merge(ep_len, ep_rew, on="_step", how="outer")
+            df_episode = pd.concat([df.set_index('episode_step') for df in episode_data_list], axis=1).reset_index()
+
+            # Training data
+            learning_data_list = [
+                get_columns(run, ["rollout/ep_len_mean", "global_step"]),
+                get_columns(run, ["rollout/ep_rew_mean", "global_step"])
+            ]
+
+            df_learning = pd.concat([df.set_index('global_step') for df in learning_data_list], axis=1).reset_index()
 
             # Add information to data
-            for df in [history, rollout]:
+            for df in [df_episode, df_learning]:
                 df["algorithm"] = run.config["algorithm"].upper()
                 df["reward_type"] = run.config["reward_type"]
                 df["run"] = run.name
 
-            df_tracking = pd.concat([df_tracking, history])
-            df_rollout = pd.concat([df_rollout, rollout])
+            df_all_episode = pd.concat([df_all_episode, df_episode])
+            df_all_learning = pd.concat([df_all_learning, df_learning])
 
-        df_tracking.to_csv(file_path)
-        df_rollout.to_csv(file_path_rollout)
+        df_all_episode.to_csv(file_path_episode)
+        df_all_learning.to_csv(file_path_learning)
 
-        print(f"Saved {file_path}")
+        print(f"Saved {file_path_episode} and {file_path_learning}")
 
 
 get_wandb_data("best_reward_square")
