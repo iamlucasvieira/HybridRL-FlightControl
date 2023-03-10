96f361b222e839ce0ab60d6cb48758eccf4d14b1
lucas <lucas6eng@gmail.com>
2023-03-10 10:01:13 +0100

    Add conda env tomox file

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,31 @@
 import nox
+from nox.sessions import Session
+
+
+def _setup(session: Session) -> None:
+    """Install `av2` into a virtual environment.
+    Args:
+        session: `nox` session.
+    """
+    session.run(
+        'conda',
+        'env',
+        'update',
+        '--prefix',
+        session.virtualenv.location,
+        '--file',
+        "environment.yml",
+        # options
+        silent=False)
+
 
 @nox.session(name='lint')
 def lint(session):
-    session.install('flake8', ".", "mypy")
+    _setup(session)
     session.run('mypy', "src")
+
+
+@nox.session(name='test')
+def test(session):
+    _setup(session)
+    session.run('pytest', "tests")
