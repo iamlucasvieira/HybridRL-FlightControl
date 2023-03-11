80d615adbfbb1ea7daf7a2286184e0095f7c2c92
lucas <lucas6eng@gmail.com>
2023-03-11 20:21:36 +0100

    Add functions for freezing tensor

--- src/helpers/torch.py
+++ src/helpers/torch.py
@@ -2,6 +2,7 @@
 
 import torch as th
 from torch import nn
+from typing import Union, Tuple
 
 
 def mlp(sizes, activation=nn.ReLU, output_activation=nn.Identity):
@@ -11,6 +12,7 @@ def mlp(sizes, activation=nn.ReLU, output_activation=nn.Identity):
         act = activation if j < len(sizes) - 2 else output_activation
         layers += [th.nn.Linear(sizes[j], sizes[j + 1]), act()]
     return nn.Sequential(*layers)
+
 
 def get_device():
     """Get device to use for PyTorch."""
@@ -25,3 +27,32 @@ def get_device():
     device = "mps" if has_mps else "gpu" if has_gpu else "cpu"
 
     return device
+
+
+def to_tensor(*arrays, data_type=th.float32) -> Union[th.Tensor, Tuple[th.Tensor]]:
+    """Convert numpy arrays to PyTorch tensors.
+
+    args:
+        arrays: Numpy arrays to convert.
+        data_type: Data type to use for the tensors.
+    """
+    tensors = tuple(th.as_tensor(a, dtype=data_type) for a in arrays)
+    if len(tensors) == 1:
+        return tensors[0]
+    return tensors
+
+
+def update_requires_grad(module: nn.Module, requires_grad: bool):
+    """Update the requires_grad attribute of the parameters of the given module."""
+    for param in module.parameters():
+        param.requires_grad = requires_grad
+
+
+def freeze(module: nn.Module):
+    """Freeze the parameters of the given module."""
+    update_requires_grad(module, False)
+
+
+def unfreeze(module: nn.Module):
+    """Unfreeze the parameters of the given module."""
+    update_requires_grad(module, True)
