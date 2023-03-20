c34080183e384988f1fe19f228ad09ce24471515
lucas <lucas6eng@gmail.com>
2023-03-20 13:24:58 +0100

    Start editing docker

--- noxfile.py
+++ noxfile.py
@@ -1,31 +1,17 @@
 import nox
 from nox.sessions import Session
-
-
-def _setup(session: Session) -> None:
-    """Install `av2` into a virtual environment.
-    Args:
-        session: `nox` session.
-    """
-    session.run(
-        'conda',
-        'env',
-        'update',
-        '--prefix',
-        session.virtualenv.location,
-        '--file',
-        "environment.yml",
-        # options
-        silent=False)
+from typing import Any
+import tempfile
 
 
 @nox.session(name='lint')
 def lint(session):
-    _setup(session)
-    session.run('mypy', "src")
+    session.run("poetry", "install", external=True)
+    session.run("poetry", "run", 'mypy', "src")
 
 
-@nox.session(name='test')
-def test(session):
-    _setup(session)
-    session.run('pytest', "tests")
+@nox.session()
+def tests(session: Session) -> None:
+    """Run the test suite."""
+    session.run("poetry", "install", external=True)
+    session.run("poetry", "run", "pytest", external=True)
